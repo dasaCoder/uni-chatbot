7feca32104e83400a3a43811b2b43bd4df2c8daa
dilushad <dilushad@zone24x7.lk>
2020-10-02 01:07:13 +0530

    add today_lec,arrange lec

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ def create_task():
 
 
 from sklearn.naive_bayes import MultinomialNB
+from sklearn import tree
 from sklearn.feature_extraction.text import CountVectorizer
 
-classifier = MultinomialNB()
+classifier = tree.DecisionTreeClassifier(max_depth=5)
+stop_words = ['i','please','a','to']
 
-countervect = CountVectorizer(min_df=5)
+countervect = CountVectorizer(min_df=5)  ##,stop_words=stop_words
 vectorized_text = pd.DataFrame(countervect.fit_transform(dataset['text']).toarray(),columns=countervect.get_feature_names(), index=None)
 
 from sklearn.model_selection import train_test_split
@@ -51,8 +53,8 @@ def predict(text):
 
   guess_proba = classifier.predict_proba(prepare_text(text))
   max_value = max(guess_proba[0])
-
-  if max_value > 0.5:
+  print(max_value)
+  if max_value > 0.2:
     return {'result': classifier.predict(prepare_text(text))[0]}
   else: 
     return {'result' : "NOT_FOUND"}
